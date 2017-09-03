2861168721eb99cab0b1c4c052ecd60ec54dfcc1
skhati01 <30451156+skhati01@users.noreply.github.com>
2017-09-03 16:16:31 +1000

    Add files via upload

---
+++ AdhocTicketFactory.java
@@ -0,0 +1,15 @@
+package bcccp.tickets.adhoc;
+
+/**
+ *
+ * @author Sushila
+ */
+public class AdhocTicketFactory extends AdhocTicketFactory {
+
+	public IAdhocTicket make(String carparkId, int ticketNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
