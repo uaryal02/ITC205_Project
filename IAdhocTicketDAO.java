130bb5c012003f9115d8b4c1763ce871feb04f86
skhati01 <30451156+skhati01@users.noreply.github.com>
2017-09-03 23:21:23 +1000

    Update IAdhocTicketDAO.java

--- IAdhocTicketDAO.java
+++ IAdhocTicketDAO.java
@@ -3,15 +3,8 @@ package bcccp.tickets.adhoc;
 import java.util.List;
 
 public interface IAdhocTicketDAO {
-	
-    /**
-     *
-     * @param carparkId
-     * @return
-     */
-    public IAdhocTicket createTicket(String carparkId);
-	public IAdhocTicket findTicketByBarcode(String barcode);
+        public IAdhocTicket createTicket(String carparkId);
+        public IAdhocTicket findTicketByBarcode(String barcode);
 	public List<IAdhocTicket> getCurrentTickets();
-
-
 }
+
