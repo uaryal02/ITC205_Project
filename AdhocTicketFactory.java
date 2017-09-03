b71965b1d9ed460cfb4466567392bfdf929907fe
skhati01 <30451156+skhati01@users.noreply.github.com>
2017-09-03 23:17:57 +1000

    Update AdhocTicketFactory.java

--- AdhocTicketFactory.java
+++ AdhocTicketFactory.java
@@ -1,15 +1,11 @@
 package bcccp.tickets.adhoc;
 
-/**
- *
- * @author Sushila
- */
-public class AdhocTicketFactory extends AdhocTicketFactory {
+public class AdhocTicketFactory implements IAdhocTicketFactory {
 
+	@Override
 	public IAdhocTicket make(String carparkId, int ticketNo) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-
 }
+
