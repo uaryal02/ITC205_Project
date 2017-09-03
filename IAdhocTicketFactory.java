8f9651eb2b1d9c1d9860313ed980ffeda326ac9d
skhati01 <30451156+skhati01@users.noreply.github.com>
2017-09-03 23:23:03 +1000

    Update IAdhocTicketFactory.java

--- IAdhocTicketFactory.java
+++ IAdhocTicketFactory.java
@@ -2,7 +2,5 @@ package bcccp.tickets.adhoc;
 
 public interface IAdhocTicketFactory {
 	
-	public IAdhocTicket make(String carparkId, int ticketNo);
-
-
+	public IAdhocTicket make(String carparkId, int ticketNo);		
 }
