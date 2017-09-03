d21d9b478b6d2ecd6fb27871aeece8e093e41da8
skhati01 <30451156+skhati01@users.noreply.github.com>
2017-09-03 16:06:09 +1000

    Update AdhocTicket.java

--- AdhocTicket.java
+++ AdhocTicket.java
@@ -1,16 +1,14 @@
 package bcccp.tickets.adhoc;
 
-import java.util.Date;
-
-public class AdhocTicket implements IAdhocTicket {
+public class AdhocTicket {
 	
-	public int carparkId;
-	public int ticketNo;
-	public long entryDateTime;
-	public long paidDateTime;
-	public long exitDateTime;
-	public float charge;
-	public short barcode_;
+	private String carparkId;
+	private int ticketNo;
+	private long EntryDateTime;
+	private long PaidDateTime;
+	private long exitDateTime;
+	private float charge;
+	private String barcode;
 
 	
 	
@@ -19,91 +17,78 @@ public class AdhocTicket implements IAdhocTicket {
 	}
 
 
-	@Override
 	public int getTicketNo() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 
-	@Override
-	public short getBarcode() {
+	public String getBarcode() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 
-	@Override
 	public String getCarparkId() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 
-	@Override
 	public void enter(long dateTime) {
 		// TODO Auto-generated method stub
 		
 	}
 
 
-	@Override
 	public long getEntryDateTime() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 
-	@Override
 	public boolean isCurrent() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 
-	@Override
 	public void pay(long dateTime, float charge) {
 		// TODO Auto-generated method stub
 		
 	}
 
 
-	@Override
 	public long getPaidDateTime() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 
-	@Override
 	public boolean isPaid() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 
-	@Override
 	public float getCharge() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 
-	@Override
 	public void exit(long dateTime) {
 		// TODO Auto-generated method stub
 		
 	}
 
 
-	@Override
 	public long getExitDateTime() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 
-	@Override
 	public boolean hasExited() {
 		// TODO Auto-generated method stub
 		return false;
