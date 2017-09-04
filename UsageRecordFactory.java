f18b62d93bfd7b1238f5d3044ddcb4fe2cd8ab4d
skhati01 <30451156+skhati01@users.noreply.github.com>
2017-09-04 14:17:44 +1000

    Update UsageRecordFactory.java

--- UsageRecordFactory.java
+++ UsageRecordFactory.java
@@ -3,7 +3,7 @@ package bcccp.tickets.season;
 public class UsageRecordFactory implements IUsageRecordFactory {
 
 	@Override
-	public IUsageRecord make(String ticketId, long startDateTime) {
+	public IUsageRecord make(String ticketId_, long startDateTime_) {
 		// TODO Auto-generated method stub
 		return null;
 	}
